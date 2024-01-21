4cc9b4401637d2db5c4a74c7c3e24bfd11232648
Vram Voskanyan <vram.arm@gmail.com>
2024-01-21 13:42:11 +0400

    -- available in mavenCentral
    //https://central.sonatype.com/artifact/io.github.vram-voskanyan/PreviewGenerator

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-        maven("https://s01.oss.sonatype.org/content/repositories/releases/")
     }
 }
 
